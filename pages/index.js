ef89b17773831613751c6b1e663d9cef9d787e06
thechrisandrew <christopherandrew612@gmail.com>
2022-07-18 09:38:37 +0700

    Reorganized folder structure and used jsx instead of js files

--- pages/index.js
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-    return <h1 className="text-3xl">Todo App</h1>;
-}
---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import TodoForm from "../components/TodoForm";
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className="text-3xl">Todo App</h1>
+            <TodoForm></TodoForm>
+        </div>
+    );
+}
